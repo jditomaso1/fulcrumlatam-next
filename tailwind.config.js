38d1ab298f00a9c3421c4cad0f06c810e81ddbe5
jditomaso1 <211746493+jditomaso1@users.noreply.github.com>
2025-09-19 11:02:03 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
-import type { Config } from 'tailwindcss'
-const config: Config = { content: ['./app/**/*.{js,ts,jsx,tsx}'], theme: { extend: {} }, plugins: [] }
-export default config
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: { extend: {} },
+  plugins: []
+};
